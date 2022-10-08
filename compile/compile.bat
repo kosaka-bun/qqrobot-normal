66889ede16454c9e51c329d4bd2278e6947be57a
Kosaka Bun <kosaka-bun@qq.com>
2022-10-08 23:02:21 +0800

    添加exe4j编译打包脚本

---
+++ compile/compile.bat
@@ -0,0 +1,11 @@
+set PROJECT_NAME="qqrobot-normal"
+
+cd ..
+del /s /q .\build\libs\*
+for /r .\build\libs /d %%a in (*) do rmdir /s /q "%%a"
+call gradlew bootJar
+cd compile
+
+copy "%PROJECT_NAME%.exe4j" ..\build\libs\
+cd ..\build\libs
+exe4jc "%PROJECT_NAME%.exe4j"
