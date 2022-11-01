32e14543e8d368078dc4cd7e0f94ff8f32ecc103
Kosaka Bun <kosaka-bun@qq.com>
2022-11-01 23:58:20 +0800

    Modify exe4j

--- compile/compile.bat
+++ compile/compile.bat
@@ -1,8 +1,12 @@
+:: 此文件必须在compile目录下运行！
 set PROJECT_NAME="qqrobot-normal"
 
+:: 清理输出目录
 cd ..
 del /s /q .\build\libs\*
 for /r .\build\libs /d %%a in (*) do rmdir /s /q "%%a"
+
+:: 构建
 call gradlew bootJar
 cd compile
 
